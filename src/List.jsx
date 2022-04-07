686e57fd6255c51f8a2ac67c315032201b971f07
Trung911 <dangquang120595@gmail.com>
2022-04-07 22:04:37 +0700

    check reponsive

--- src/List.jsx
+++ src/List.jsx
@@ -3,38 +3,36 @@ import './App.css'
 import { STAFFS } from './staffs'
 import InfoStaff from './InfoStaff'
 import React, { useState } from 'react';
+import { Fragment } from 'react/cjs/react.development';
 
 function List() {
     const [staffId, setStaffId] = useState(null);
-    const [col, setCol ] = useState(3);
-
-  
+    const [col, setCol] = useState(3);
     return (
-        <Container id='list'>
-            <div className='container'>
-  <Button color="primary" outline onClick={() => setCol(2)}>
-    2  Cột
-  </Button>
-  <Button color="primary" outline onClick={() => setCol(3)}>
-    3 Cột
-  </Button>
-  <Button color="primary" outline onClick={() => setCol(6)}>
-    6  Cột
-  </Button>
-</div>
-<div ></div>
-            <Row xs={col} md={col} lg= {col}>
-                {STAFFS.map(STAFF => (
-                    <Col className="bg-light border list-text" key={STAFF.id} onClick={() => setStaffId(STAFF.id)}>
-                        {STAFF.name}
-                    </Col>
-                ))}
-            </Row>
+        <Fragment>
+            <Container id='list'>
+                <div className='container button-group'>
+                    <Button color="primary" outline onClick={() => setCol(2)}>
+                        2  Cột
+                    </Button>
+                    <Button color="primary" outline onClick={() => setCol(3)}>
+                        3 Cột
+                    </Button>
+                    <Button color="primary" outline onClick={() => setCol(6)}>
+                        6  Cột
+                    </Button>
+                </div>
+                <Row xs="1" md={col} lg={col}>
+                    {STAFFS.map(STAFF => (
+                        <Col className="bg-light border list-text" key={STAFF.id} onClick={() => setStaffId(STAFF.id)}>
+                            {STAFF.name}
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
             <InfoStaff staffId={staffId} />
-        </Container>
-
+        </Fragment>
     )
 }
-
 export default List
 
